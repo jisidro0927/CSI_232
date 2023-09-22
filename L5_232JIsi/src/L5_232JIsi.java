a9a4dd07734d5ef2f6760a7c48a711c0a0b94b54
jisidro0927 <james.isidro0927@gmail.com>
2023-09-22 15:29:12 -0700

    update L5_232JIsi/src/L5_232JIsi.java

---
+++ L5_232JIsi/src/L5_232JIsi.java
@@ -0,0 +1,15 @@
+/*
+    James Isidro / james_isidro@my.cuesta.edu
+    CIS 232 / Scovil
+    Lab 5
+ */
+ 
+ public class L5_232JIsi
+ {
+   
+   
+   public static void main(String[] args
+   {
+     
+   }
+ }
