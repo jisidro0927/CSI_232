064fe6db45654908fbeed0a91afe95fb4352f920
jisidro0927 <james.isidro0927@gmail.com>
2023-09-29 15:03:31 -0700

    A1232_JIsi
    started A1232JIsi

--- A1232JIsi/src/A1232JIsi.java
+++ A1232JIsi/src/A1232JIsi.java
@@ -1,6 +1,12 @@
 import weiss.util.*;
 
 
-public class A1232JIsi {
+public class A1232JIsi<AnyType extends Comparable<? super AnyType>> extends ArrayList<AnyType> {
 
+    @Override
+    public boolean add(AnyType x) {
+
+
+        return super.add(x);
+    }
 }
